5ae0b9ef7a0ba4fb9e5eafb5bcd38f60a7f02092
Mark Jones <mark.jones@enta.net>
2018-09-11 11:54:37 +0100

    Kafka event initial commit
    Everything needed currently to make kafka events work. No testing or proper readme file at the moment.

---
+++ src/KafkaEvent.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markjones
+ * Date: 10/09/18
+ * Time: 14:57
+ */
+
+class KafkaEvent
+{
+
+}
---
+++ src/kafkaEventHelper.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markjones
+ * Date: 10/09/18
+ * Time: 14:58
+ */
